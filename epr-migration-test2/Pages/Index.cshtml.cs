30f15b6d4335caa69092b3aff62168b409e0ce43
Michael Johnson <michael.johnson.external@atos.net>
2024-02-21 15:54:26 +0000

    Add project files.

---
+++ epr-migration-test2/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace epr_migration_test2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
